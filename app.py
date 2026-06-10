b52bdcdd448e1ae392a8cb78b8920aa1f9cce9e4
JagratiS <62231210+JagratiS259@users.noreply.github.com>
2026-06-10 15:02:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.chains import RetrievalQA
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 
